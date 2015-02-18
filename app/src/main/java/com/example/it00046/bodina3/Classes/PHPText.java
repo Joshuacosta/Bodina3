f3ca8eb9655785095ca8a09443819057cbb13816
Joshuacosta <joshuacosta6@gmail.com>
2015-02-18 18:44:00 +0100

    Feina de avui 12/02/2015

---
+++ app/src/main/java/com/example/it00046/bodina3/Classes/PHPText.java
@@ -0,0 +1,28 @@
+package com.example.it00046.bodina3.Classes;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+/**
+ * Created by it00046 on 18/02/2015.
+ */
+public class PHPText {
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+
+
+
+
+    public static void get(String url, RequestParams params, TextHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, TextHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return Globals.k_DirectoriPHP + relativeUrl;
+    }
+}
