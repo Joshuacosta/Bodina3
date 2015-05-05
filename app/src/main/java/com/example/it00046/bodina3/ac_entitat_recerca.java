99420100ea5e8636fbae5da9348e80b7e6086cd6
Joshuacosta <joshuacosta6@gmail.com>
2015-05-05 18:20:23 +0200

    Feina de avui 10/03/2015

--- app/src/main/java/com/example/it00046/bodina3/ac_entitat_recerca.java
+++ app/src/main/java/com/example/it00046/bodina3/ac_entitat_recerca.java
@@ -1,39 +1,62 @@
 package com.example.it00046.bodina3;
 
+import android.app.Activity;
 import android.app.SearchManager;
+import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 
-public class ac_entitat_recerca extends ActionBarActivity {
+public class ac_entitat_recerca extends Activity {
+
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ly_entitat_recerca);
+
+        resultText = (TextView)findViewById(R.id.searchViewResult);
+
+        setupSearchView();
+    }
+
+    private void setupSearchView() {
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(searchableInfo);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.mn_entitat_recerca, menu);
+    protected void onNewIntent(Intent intent) {
+        if (ContactsContract.Intents.SEARCH_SUGGESTION_CLICKED.equals(intent.getAction())) {
+            //handles suggestion clicked query
+            String displayName = getDisplayNameForContact(intent);
+            resultText.setText(displayName);
+        } else if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            // handles a search query
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            resultText.setText("should search for query: '" + query + "'...");
+        }
+    }
 
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        // Assumes current activity is the searchable activity
-        // Aixó dona error!!!!!!!!!!!!!!!!!! searchView es null
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-
-        return true;
+    private String getDisplayNameForContact(Intent intent) {
+        Cursor phoneCursor = getContentResolver().query(intent.getData(), null, null, null, null);
+        phoneCursor.moveToFirst();
+        int idDisplayName = phoneCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+        String name = phoneCursor.getString(idDisplayName);
+        phoneCursor.close();
+        return name;
     }
 
     @Override
