f02fe17076b4bc8e4ac71917ac47dba1ad09aec6
Joshuacosta <joshuacosta6@gmail.com>
2015-08-04 18:01:56 +0200

    Feina de avui 10/06/2015

--- app/src/main/java/com/example/it00046/bodina3/Classes/SQLDB.java
+++ app/src/main/java/com/example/it00046/bodina3/Classes/SQLDB.java
@@ -20,9 +20,10 @@ public class SQLDB extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Definim les taules de la BBDD
+        String l_Create;
+        // Definim les taules de la BBDD local
         // Client
-        String CREATE = String.format(Globals.g_Native.getString(R.string.TClientCreate),
+        l_Create = String.format(Globals.g_Native.getString(R.string.TClientCreate),
                 Globals.g_Native.getString(R.string.TClient),
                 Globals.g_Native.getString(R.string.TClient_Codi),
                 Globals.g_Native.getString(R.string.TClient_eMail),
@@ -32,13 +33,83 @@ public class SQLDB extends SQLiteOpenHelper {
                 Globals.g_Native.getString(R.string.TClient_DataAlta),
                 Globals.g_Native.getString(R.string.TClient_Idioma),
                 Globals.g_Native.getString(R.string.TClient_Actualitzat));
-        db.execSQL(CREATE);
+        db.execSQL(l_Create);
+        // Salons
+        l_Create = String.format(Globals.g_Native.getString(R.string.TSalons),
+                Globals.g_Native.getString(R.string.TSalons_Codi),
+                Globals.g_Native.getString(R.string.TSalons_Nom),
+                Globals.g_Native.getString(R.string.TSalons_Amplada),
+                Globals.g_Native.getString(R.string.TSalons_Alsada),
+                Globals.g_Native.getString(R.string.TSalons_CodiPlanol),
+                Globals.g_Native.getString(R.string.TSalons_Estat));
+        db.execSQL(l_Create);
+        // Tipus de celebracio
+        l_Create = String.format(Globals.g_Native.getString(R.string.TTipusCelebracio),
+                Globals.g_Native.getString(R.string.TTipusCelebracio_Codi),
+                Globals.g_Native.getString(R.string.TTipusCelebracio_Descripcio));
+        db.execSQL(l_Create);
+        // Planols
+        l_Create = String.format(Globals.g_Native.getString(R.string.TPlanols),
+                Globals.g_Native.getString(R.string.TPlanols_Codi),
+                Globals.g_Native.getString(R.string.TPlanols_OrigenX),
+                Globals.g_Native.getString(R.string.TPlanols_OrigenY),
+                Globals.g_Native.getString(R.string.TPlanols_DestiX),
+                Globals.g_Native.getString(R.string.TPlanols_DestiY),
+                Globals.g_Native.getString(R.string.TPlanols_Angle));
+        db.execSQL(l_Create);
+        // Celebracions
+        l_Create = String.format(Globals.g_Native.getString(R.string.TCelebracions),
+                Globals.g_Native.getString(R.string.TCelebracions_Codi),
+                Globals.g_Native.getString(R.string.TCelebracions_CodiSalo),
+                Globals.g_Native.getString(R.string.TCelebracions_Tipus),
+                Globals.g_Native.getString(R.string.TCelebracions_Descripcio),
+                Globals.g_Native.getString(R.string.TCelebracions_Convidats),
+                Globals.g_Native.getString(R.string.TCelebracions_Data),
+                Globals.g_Native.getString(R.string.TCelebracions_Lloc),
+                Globals.g_Native.getString(R.string.TCelebracions_Contacte),
+                Globals.g_Native.getString(R.string.TCelebracions_Estat));
+        db.execSQL(l_Create);
+        // Distribucions
+        l_Create = String.format(Globals.g_Native.getString(R.string.TDistribucions),
+                Globals.g_Native.getString(R.string.TDistribucions_CodiCelebracio),
+                Globals.g_Native.getString(R.string.TDistribucions_CodiDistribucio),
+                Globals.g_Native.getString(R.string.TDistribucions_Nom),
+                Globals.g_Native.getString(R.string.TDistribucions_DataAlta),
+                Globals.g_Native.getString(R.string.TDistribucions_DataModificacio),
+                Globals.g_Native.getString(R.string.TDistribucions_Estat));
+        db.execSQL(l_Create);
+        // Distribucio Taules
+        l_Create = String.format(Globals.g_Native.getString(R.string.TDistribucioTaules),
+                Globals.g_Native.getString(R.string.TDistribucioTaules_CodiCelebracio),
+                Globals.g_Native.getString(R.string.TDistribucioTaules_CodiDistribucio),
+                Globals.g_Native.getString(R.string.TDistribucioTaules_NumTaula),
+                Globals.g_Native.getString(R.string.TDistribucioTaules_CodiTaula),
+                Globals.g_Native.getString(R.string.TDistribucioTaules_Descripcio),
+                Globals.g_Native.getString(R.string.TDistribucioTaules_PosicioX),
+                Globals.g_Native.getString(R.string.TDistribucioTaules_PosicioY));
+        db.execSQL(l_Create);
+        // Distribucio Convidats
+        l_Create = String.format(Globals.g_Native.getString(R.string.TDistribucioConvidats),
+                Globals.g_Native.getString(R.string.TDistribucioConvidats_CodiCelebracio),
+                Globals.g_Native.getString(R.string.TDistribucioConvidats_CodiDistribucio),
+                Globals.g_Native.getString(R.string.TDistribucioConvidats_CodiConvidat),
+                Globals.g_Native.getString(R.string.TDistribucioConvidats_PosicioX),
+                Globals.g_Native.getString(R.string.TDistribucioConvidats_PosicioY),
+                Globals.g_Native.getString(R.string.TDistribucioConvidats_NumTaula));
+        db.execSQL(l_Create);
+        // Categories de convidats
+        l_Create = String.format(Globals.g_Native.getString(R.string.TCategoriesConvidats),
+                Globals.g_Native.getString(R.string.TCategoriesConvidats_Codi),
+                Globals.g_Native.getString(R.string.TCategoriesConvidats_CodiCelebracio),
+                Globals.g_Native.getString(R.string.TCategoriesConvidats_Descripcio));
+        db.execSQL(l_Create);
+        // Menus de convidats
+        l_Create = String.format(Globals.g_Native.getString(R.string.TMenusConvidats),
+                Globals.g_Native.getString(R.string.TMenusConvidats_Codi),
+                Globals.g_Native.getString(R.string.TMenusConvidats_Descripcio));
+        db.execSQL(l_Create);
         // Si executem aixó es que hem creat la BBDD i no hi han dades, de moment:
         Globals.g_NoHiHanDades = true;
-        //
-        // Experiment per recuperar el identificador a partir de un texte
-        //int aux = Globals.g_Native.getResources().getIdentifier("TClientCreate", "string", "com.example.it00046.bodina3");
-        //String auxc = Globals.g_Native.getString(aux);
         //
     }
 
