e5c490ea5097e65f86dbadd86e8506b7d52e86c0
Joshuacosta <joshuacosta6@gmail.com>
2015-09-30 18:33:23 +0200

    Feina de avui 10/06/2015

---
+++ app/src/main/java/com/example/it00046/bodina3/Classes/CanvasSimple.java
@@ -0,0 +1,74 @@
+package com.example.it00046.bodina3.Classes;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.util.AttributeSet;
+import android.view.View;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+
+public class CanvasSimple extends View {
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+    //
+    private int g_CenterX;
+    private int g_CenterY;
+
+    public CanvasSimple(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    public void Dibuixa(int p_Amplada, int p_Alsada){
+        startNew();
+        drawCanvas.drawRect(g_CenterX - (p_Amplada / 2),
+                g_CenterY - (p_Alsada / 2),
+                g_CenterX + (p_Amplada / 2), g_CenterY + (p_Alsada / 2), canvasPaint);
+        invalidate();
+    }
+
+    public void startNew(){
+        drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        invalidate();
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        //view given size
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+        //
+        g_CenterX = w/2;
+        g_CenterY = h/2;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //draw view
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+}
--- app/src/main/java/com/example/it00046/bodina3/salons_client_alta.java
+++ app/src/main/java/com/example/it00046/bodina3/salons_client_alta.java
@@ -1,41 +1,104 @@
 package com.example.it00046.bodina3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.it00046.bodina3.Classes.CanvasSimple;
+import com.example.it00046.bodina3.Classes.DAO.DAOSalonsClient;
+import com.example.it00046.bodina3.Classes.Entitats.SaloClient;
+import com.example.it00046.bodina3.Classes.Globals;
+
+import java.util.Comparator;
 
 /**
  * Created by it00046 on 29/09/2015.
  */
 public class salons_client_alta extends ActionBarActivity {
+    private CanvasSimple g_Canvas;
+    private int g_Amplada = 0, g_Alsada = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SeekBar l_SEK_Amplada, l_SEK_Alsada;
+        final TextView l_TXT_Amplada, l_TXT_Alsada;
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.celebracions_client_alta);
+        setContentView(R.layout.salons_client_alta);
+        // Recuperem els controls
+        l_SEK_Amplada = (SeekBar) findViewById(R.id.SalonsClientAltaSEKAmplada);
+        l_SEK_Alsada = (SeekBar) findViewById(R.id.SalonsClientAltaSEKAlsada);
+        l_TXT_Amplada = (TextView) findViewById(R.id.SalonsClientAltaLITAmplada);
+        l_TXT_Alsada = (TextView) findViewById(R.id.SalonsClientAltaLITAlsada);
+        g_Canvas = (CanvasSimple) findViewById(R.id.SalonsClientAltaVIWDrawing);
+        // Codi de les seekbars
+        l_SEK_Amplada.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int l_AmpladaTriada = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                l_AmpladaTriada = progresValue;
+                g_Amplada = l_AmpladaTriada;
+                l_TXT_Amplada.setText(l_AmpladaTriada + " " + Globals.g_Native.getString(R.string.meters));
+                if (g_Alsada != 0){
+                    g_Canvas.Dibuixa(g_Amplada * 5, g_Alsada * 5);
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //l_TXT_Amplada.setText(seekBar.getMax());
+                g_Amplada = l_AmpladaTriada;
+            }
+        });
+        l_SEK_Alsada.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int l_AlsadaTriada = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                l_AlsadaTriada = progresValue;
+                g_Alsada = l_AlsadaTriada;
+                l_TXT_Alsada.setText(l_AlsadaTriada + " " + Globals.g_Native.getString(R.string.meters));
+                if (g_Amplada != 0) {
+                    g_Canvas.Dibuixa(g_Amplada * 5, g_Alsada * 5);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                g_Alsada = l_AlsadaTriada;
+            }
+        });
+        // Mostrem el tornar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.celebracions_client_alta, menu);
+        getMenuInflater().inflate(R.menu.salons_client_alta, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public boolean onOptionsItemSelected(MenuItem p_Item) {
+        Intent l_Intent;
+        int l_id = p_Item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(p_Item);
     }
+
 }
